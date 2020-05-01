5746ae7e894edda8105ba9f4364658aa571f05c7
Jakob Kallin <jakobkallin@gmail.com>
2020-05-01 11:41:02 +0200

    Reorganize/reformat
    Mostly changes to comments; no changes to functionality.

--- server.py
+++ server.py
@@ -26,15 +26,10 @@ board = [
     ['rw', 'hw', 'bw', 'kw', 'qw', 'bw', 'hw', 'rw']
 ]
 
-
-
-
 # When the user visits the index page, we just show the game's HTML.
-
 @app.route('/')
 def http_index():
     return render_template('index.html')
-
 
 # Every time the user tries to make a move, the client sends an HTTP request to this endpoint. It will contain two URL parameters:
 #
@@ -42,18 +37,18 @@ def http_index():
 # - to: The square to which the user tries to move (A1 notation)
 #
 # This method should check whether the move is valid, and then update the board so that the piece has moved and possibly removed another piece at the same time.
-
 def translate_notation(board_square):
     # convert Alphabet column to integer
     # take the first char of board_square e.g ('A1')
     letter = board_square[0]
     board_square_column = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
-
     column = board_square_column[letter]
+
     # take the second char of e.g ('A1')
     number = board_square[1]
     board_square_row = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
     row = board_square_row[number]
+
     return row, column
 
 def king(from_row, from_column, to_row, to_column, player, board):
@@ -73,13 +68,12 @@ def king(from_row, from_column, to_row, to_column, player, board):
                 # If the piece is an enemy.
                 king_color = board[from_row][from_column][1]
                 if piece is not None and piece[1] != king_color:
-                    # If other piece can attack king...
                     if player == 'white':
                         other_player = 'black'
                     else:
                         other_player = 'white'
+                    # If the enemy can attack the king.
                     can_attack_king = valid_movement(other_piece_row, other_piece_column, to_row, to_column, other_player, board)
-                    # If the enemy can attack us, return false.
                     if can_attack_king:
                         return False
         # If nothing is returned inside the loop, we will get here, and that means no enemy can attack us.
@@ -115,13 +109,11 @@ def bishop(from_row, from_column, to_row, to_column, board):
             elif to_row < from_row and to_column < from_column:
                 row = row - 1
                 column = column - 1
-                
             
             #direction is 'bottom_left'
             elif to_row > from_row and to_column < from_column:
                 row = row + 1
                 column = column - 1
-                
             
             #direction is 'bottom_right'
             elif to_row > from_row and to_column > from_column:
@@ -129,6 +121,7 @@ def bishop(from_row, from_column, to_row, to_column, board):
                 column = column + 1
 
             square_in_between.append(board[row][column])    
+
         # square in between except the last element. We don't need the last element which is the destination
         square_in_between = square_in_between[:-1]
         if all(square is None for square in square_in_between):
@@ -138,8 +131,6 @@ def bishop(from_row, from_column, to_row, to_column, board):
     # if not diagonal
     else:
         return False
-
-
         
 def rook(from_row, from_column, to_row, to_column, board):
     # (horizontal movement): rook can moves in these columns 
@@ -163,12 +154,12 @@ def rook(from_row, from_column, to_row, to_column, board):
             square_in_between = column_of_rook[to_row+1 : from_row]
     else:
         return False
+
     # if all the element in the square_in_between is None. In the other words if there is any piech in the way of rook
     if all(piece is None for piece in square_in_between):
         return True
     else:
         return False
-
 
 def pawn(from_row, from_column, to_row, to_column, player, board):
     # for white
@@ -204,7 +195,6 @@ def pawn(from_row, from_column, to_row, to_column, player, board):
             return False
 
     ### rules for black pawn:
-
     # else if player is black:
     # if black pawn is at the first step, can move two steps
     if player == 'black':
@@ -228,7 +218,6 @@ def pawn(from_row, from_column, to_row, to_column, player, board):
     else:
         return False
 
-
 def horse(from_row, from_column, to_row, to_column, board):
     # All possible moves of a horse
     squares = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
@@ -243,7 +232,6 @@ def horse(from_row, from_column, to_row, to_column, board):
             return True
     return False
 
-
 def attack_friend(from_row, from_column, to_row, to_column, board):
     destination_position = board[to_row][to_column]
     piece = board[from_row][from_column]
@@ -256,6 +244,7 @@ def attack_friend(from_row, from_column, to_row, to_column, board):
         return True
 
 # This function checks if the piece at from_row/from_column can move to to_row/to_column, based on which type of piece it is.
+# It's the starting point of the most important and complex logic in the game.
 def valid_movement(from_row, from_column, to_row, to_column, player, board):
     # We don't have the piece variable in here but we can easily create it again.
     piece = board[from_row][from_column]
@@ -289,9 +278,6 @@ def http_move():
     # take the piece which want to move
     piece = board[from_row][from_column]
 
-    # If the user tries to make an invalid move, we should return an error message and the HTTP status code 422, which is an error code.
-    # return 'Other player should play!', 422
-
     # (e.g piece = 'pw', 'bw').... player['white'], player['black']
     # If one player, play two times
     if piece[1] != player[0]:
@@ -303,7 +289,7 @@ def http_move():
         return 'You cannot attack your friend', 422
 
     # This code checks the movement rules for all different pieces.
-    # We can put it in a separate function, so we can easily reuse it in the king function!
+    # We put it in a separate function, so that we can easily reuse it in the king function!
     valid = valid_movement(from_row, from_column, to_row, to_column, player, board)
 
     if valid:
@@ -326,8 +312,7 @@ def http_move():
         'board': board
     }
 
-
-# If the user reloads the page (or closes it by mistake and reopens), the client will send a request to this endpoint in order to see the current state of the game and put the pieces where they were before the page was reloaded or closed. This function does not need to be changed.
+# The state function is retrieved every second by the client (through polling) so that both players see each other's moves.
 @app.route('/state')
 def http_state():
     if 'since' in request.args:
